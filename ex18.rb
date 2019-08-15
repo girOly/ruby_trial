f362b4859e4f8cc1abbe86b6dd86eaf056f14806
girOly <girardob@gmail.com>
2019-08-15 15:41:47 -0400

    More ARGV in Ruby

---
+++ ex18.rb
@@ -0,0 +1,31 @@
+# ARGV Script
+
+def print_two(*args)
+  arg1, arg2, = args
+  puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+# Alternative
+
+def print_two_again(arg1, arg2)
+  puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+#Only one Argument
+
+def print_one(arg1)
+  puts "arg1: #{arg1}"
+end
+
+
+#No arguments
+
+def print_none()
+  puts "I got nothin'."
+end
+
+
+print_two("Zed","Shaw")
+print_two_again("Zed","Shaw")
+print_one("First!")
+print_none()
